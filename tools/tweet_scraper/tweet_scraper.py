13a409b7feb3b2095fc81eb0d635451a2962063b
cortolan <corbin.orto@gmail.com>
2021-09-28 13:42:50 -0400

    moddified terminal output and script now locates itself properly

--- tools/tweet_scraper/tweet_scraper.py
+++ tools/tweet_scraper/tweet_scraper.py
@@ -8,8 +8,9 @@ from datetime import datetime
 import pickle
 
 print('dmyHMS = ',datetime.now().strftime('%d-%m-%y_%H-%M-%S'))
+FILE_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 
-with open("keys.json") as f:
+with open("{}/keys.json".format(FILE_DIRECTORY)) as f:
     keys = json.load(f)
 
 def get_hashtags():
@@ -19,21 +20,21 @@ def get_hashtags():
         hashtag_list[entry] = tag[:-1].lower()
     return hashtag_list
         
-def write_tweets(NUM_TWEETS):
+def write_tweets(num_tweets):
     current_time = datetime.now().strftime('%d-%m-%y_%H-%M-%S')
     for hashtag in get_hashtags():
-        tweets_save_path = 'tweets/{}_recent_tweets/{}_{}.pkl'.format(hashtag[1:], hashtag[1:],  current_time)
+        tweets_save_path = '{}/tweets/{}_recent_tweets/{}_{}.pkl'.format(FILE_DIRECTORY, hashtag[1:], hashtag[1:],  current_time)
         
         os.makedirs(os.path.dirname(tweets_save_path), exist_ok=True)
         auth = tweepy.OAuthHandler(keys['consumer_key'], keys['consumer_secret'])
         auth.set_access_token(keys['access_token'], keys['access_token_secret'])
         api = tweepy.API(auth, wait_on_rate_limit=True)
-        tweets = list(tweepy.Cursor(api.search, q=hashtag, rpp=100, tweet_mode='extended').items(NUM_TWEETS))
-        print('API accessed successfully')
+        tweets = list(tweepy.Cursor(api.search, q=hashtag, rpp=100, tweet_mode='extended').items(num_tweets))
+        print('Looking up {}, API accessed successfully'.format(hashtag))
 
         with open(tweets_save_path, 'wb') as f:
             pickle.dump(tweets, f)
-            print('Tweets serialized successfully')
+            print('{} tweets serialized successfully'.foramt(hashtag))
 
 def read_tweets():
     for root, dir, file, in os.walk('tweets'):
